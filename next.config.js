0a283a27378c7167d1b4dbecd2783d51c7b471a4
Restyled.io <commits@restyled.io>
2021-02-10 17:50:51 +0000

    Restyled by prettier

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withWorkbox = require('next-with-workbox');
 module.exports = withWorkbox({
-  workbox : {
-    swSrc : 'worker.js',
+  workbox: {
+    swSrc: 'worker.js',
     // .
     // ..
     // ... any workbox-webpack-plugin.GenerateSW option
